31d963185f9e18cc5623f5e929be04b19aa7de28
Vani <Vani@Vani-PC>
2018-10-13 23:13:09 +0530

    updated

--- E2ESelenium_Mavenjenkins/src/Testcases/TC_001.java
+++ E2ESelenium_Mavenjenkins/src/Testcases/TC_001.java
@@ -1,4 +1,7 @@
 package Testcases;
+import static org.testng.Assert.assertTrue;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 public class TC_001 {
@@ -10,9 +13,19 @@ public class TC_001 {
 		System.setProperty("webdriver.chrome.driver","./Driver/chromedriver.exe");
 		ChromeDriver driver = new ChromeDriver();
 		driver.get("http://www.facebook.com");
-		driver.findElementById("email").sendKeys("hello");
-		driver.findElementById("pass").sendKeys("hello");
-		driver.quit();
+		driver.findElementById("email").sendKeys("vanikodandan@gmail.com");
+		driver.findElementById("pass").sendKeys("vani@18061992");
+		driver.findElement(By.xpath("//input[@value = 'Log In']")).click();
+		assertTrue(driver.getTitle().contains("Title of Page"));
+		//String actualTitle = driver.getTitle();
+		//System.out.println(actualTitle);
+		try {
+			Thread.sleep(20000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		driver.close();
 	}
 	}
 
--- E2ESelenium_Mavenjenkins/src/Testcases/TC_002.java
+++ E2ESelenium_Mavenjenkins/src/Testcases/TC_002.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 
 public class TC_002 {
 	@Test
-	public void testcase1(){
+	public void testcase2(){
 		System.setProperty("webdriver.chrome.driver","./Driver/chromedriver.exe");
 		ChromeDriver driver = new ChromeDriver();
 		driver.get("http://www.facebook.com");
