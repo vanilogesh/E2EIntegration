b23f9931338a4011d1c82d3e349e50b87a6b6b7c
Vani <Vani@Vani-PC>
2018-10-18 00:53:30 +0530

    df

---
+++ E2ESelenium_Mavenjenkins/src/Testcases/TC_003.java
@@ -0,0 +1,29 @@
+package Testcases;
+
+import static org.testng.Assert.assertTrue;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC_003 {
+	@Test
+	public void testcase3(){
+		System.setProperty("webdriver.chrome.driver","./Driver/chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("http://www.facebook.com");
+		driver.findElementById("email").sendKeys("vanikodandan@gmail.com");
+		driver.findElementById("pass").sendKeys("vani@18061992");
+		driver.findElement(By.xpath("//input[@value = 'Log In']")).click();
+		assertTrue(driver.getTitle().contains("facebook"));
+		//String actualTitle = driver.getTitle();
+		//System.out.println(actualTitle);
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		driver.quit();
+}
+}
